1c68b0a3165de4d33eaa73cdb3418ffddc425f59
David Cheatham <sancrock@gmail.com>
2020-01-27 16:52:17 -0500

    Added support for damage modifiers

--- Program.cs
+++ Program.cs
@@ -19,27 +19,63 @@ namespace DiceRoller
                 string input = Console.ReadLine();
                 if (input.ToLower() == "q" || input.ToLower() == "quit")
                 {
+                    Console.Clear();
+                    Console.WriteLine("Program End");
                     break;
                 }
                 try
                 {
-                    int split = input.ToLower().IndexOf("d");
-                    string numDiceStr = input.Substring(0, split);
-                    int numDice = Int32.Parse(numDiceStr);
-                    string sidesStr = input.Substring(split + 1, input.Length - split -1);
-                    int sides = Int32.Parse(sidesStr);
+                    int dieSplit = input.ToLower().IndexOf("d");
+                    int numDice;
+                    int sides;
+                    int bonusSplit;
+                    int damageBonus;
+                    string sidesStr;
+
+                    if (dieSplit == 0)
+                    {
+                        numDice = 1;
+                    }
+                    else
+                    {
+                        string numDiceStr = input.Substring(0, dieSplit);
+                        numDice = Int32.Parse(numDiceStr);
+                    }
+
+                    if(input.IndexOf("+") == -1 && input.IndexOf("-") == -1)
+                    {
+                        sidesStr = input.Substring(dieSplit + 1, input.Length - dieSplit -1);
+                        damageBonus = 0;
+                    }
+                    else if (input.IndexOf("+") != -1 )
+                    {
+                        bonusSplit = input.ToLower().IndexOf("+");
+                        sidesStr = input.Substring(dieSplit + 1,bonusSplit - dieSplit -1);
+                        string damageBonusStr = input.Substring(bonusSplit + 1, input.Length - bonusSplit -1);
+                        damageBonus = Int32.Parse(damageBonusStr);
+                    }
+                    else
+                    {
+                        bonusSplit = input.ToLower().IndexOf("-");
+                        sidesStr = input.Substring(dieSplit + 1,bonusSplit - dieSplit -1);
+                        string damageBonusStr = input.Substring(bonusSplit + 1, input.Length - bonusSplit -1);
+                        damageBonus = -(Int32.Parse(damageBonusStr));
+
+                    }
+                    sides = Int32.Parse(sidesStr);
                     Console.WriteLine("");
-                    RollDice(numDice, sides);
+                    RollDice(numDice, sides, damageBonus);
                 }
                 catch (Exception)
                 {
                     Console.WriteLine($"Invalid entry, please try again.");
+                    //Console.WriteLine(ex);
                 }
                 Console.ReadKey();
             }
         }
 
-        public static void RollDice(int numDice, int sides)
+        public static void RollDice(int numDice, int sides, int damageBonus)
         {
             Die[] Dice = new Die[numDice];
 
@@ -57,7 +93,29 @@ namespace DiceRoller
             }
 
             rollText += "]";
-            Console.WriteLine($"Rolling {numDice}d{sides}, your total was {total}{rollText}");
+            total += damageBonus;
+
+            string numDiceText;
+            if (numDice == 1)
+            {
+                numDiceText = "";
+            }
+            else
+            {
+                numDiceText = $"{numDice}";
+            }
+
+            string damageBonusText = "";
+            if (damageBonus < 0)
+            {
+                damageBonusText = $"{damageBonus}";
+            }
+            if (damageBonus > 0)
+            {
+                damageBonusText = $"+{damageBonus}";
+            }
+
+            Console.WriteLine($"Rolling {numDiceText}d{sides}{damageBonusText}, your total was {total}{rollText}");
         }
     }
 }
